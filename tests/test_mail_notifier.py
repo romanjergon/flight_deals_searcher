5896372eed626429bd3bf820c0fde859c17871b2
romanjergon <roman.jergon@gmail.com>
2021-11-29 11:32:47 +0100

    Introduced safety

--- tests/test_mail_notifier.py
+++ tests/test_mail_notifier.py
@@ -20,7 +20,6 @@ def test_send_notif_mail(mocker):
         )
 
         result = notifier.send_notif_mail(
-
             "This is testing mail for mail notifier class", "Lorem Ipsum"
         )
         assert (
